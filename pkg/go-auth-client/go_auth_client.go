4487c1e246587056ea03ac30a1838bd3169180de
JamesStallworthy <james@jamesstallworthy.com>
2023-02-08 20:36:33 +0000

    Added the ability to login with the auth client

--- pkg/go-auth-client/go_auth_client.go
+++ pkg/go-auth-client/go_auth_client.go
@@ -1,7 +1,11 @@
 package goauthclient
 
 import (
+	"encoding/json"
+	"fmt"
 	goauthdocdisco "go-auth/pkg/go-auth-doc-disco"
+	"io"
+	"net/http"
 )
 
 type GoAuthClient struct {
@@ -28,6 +32,34 @@ func CreateClient(authority string) (GoAuthClient, error) {
 	}, nil
 }
 
-// func (c GoAuthClient) LoginClientCredentials(clientId string, clientSecret string) (string, error) {
+type TokenResponse struct {
+	Token string `json:"token"`
+}
 
-// }
+func (c GoAuthClient) LoginClientCredentials(clientId string, clientSecret string) (string, error) {
+	resp, err := http.Get(fmt.Sprintf("%s?clientId=%s&clientSecret=%s", c.Config.TokenEndpoint, clientId, clientSecret))
+
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("auth service returned: %s", resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	tokenResponse := TokenResponse{}
+	err2 := json.Unmarshal([]byte(body), &tokenResponse)
+
+	if err2 != nil {
+		return "", err
+	}
+
+	return tokenResponse.Token, nil
+}
--- pkg/go-auth-client/go_auth_client_test.go
+++ pkg/go-auth-client/go_auth_client_test.go
@@ -50,3 +50,69 @@ func TestCreateClientInvalid(t *testing.T) {
 
 	assert.Equal(t, "invalid Issuer", err.Error())
 }
+
+func TestLoginClient(t *testing.T) {
+	defer gock.Off() // Flush pending mocks after test execution
+
+	gock.New("http://example.com").
+		Get("/.well-known/openid-configuration").
+		Reply(200).
+		JSON(goauthdocdisco.OpenIdConfig{
+			Issuer:                 "http://example.com",
+			TokenEndpoint:          "http://example.com/oauth/oauth20/token",
+			JwksUri:                "http://example.com/oauth/jwks",
+			ScopesSupported:        []string{"openid"},
+			GrantTypesSupported:    []string{"client_credentials"},
+			ResponseTypesSupported: []string{"token"},
+			TokenEndpointsEndpointAuthSigningAlgValuesSupported: []string{"RS256"},
+		})
+
+	gock.New("http://example.com").
+		Get("/oauth/oauth20/token").
+		Reply(200).
+		JSON(TokenResponse{
+			Token: "AToken",
+		})
+
+	client, err := CreateClient("http://example.com")
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "http://example.com", client.Config.Issuer)
+
+	token, err := client.LoginClientCredentials("TempuratureClient", "SuperSecretSecret")
+
+	assert.Equal(t, nil, err)
+	assert.NotNil(t, token)
+}
+
+func TestLoginClientInvalid(t *testing.T) {
+	defer gock.Off() // Flush pending mocks after test execution
+
+	gock.New("http://example.com").
+		Get("/.well-known/openid-configuration").
+		Reply(200).
+		JSON(goauthdocdisco.OpenIdConfig{
+			Issuer:                 "http://example.com",
+			TokenEndpoint:          "http://example.com/oauth/oauth20/token",
+			JwksUri:                "http://example.com/oauth/jwks",
+			ScopesSupported:        []string{"openid"},
+			GrantTypesSupported:    []string{"client_credentials"},
+			ResponseTypesSupported: []string{"token"},
+			TokenEndpointsEndpointAuthSigningAlgValuesSupported: []string{"RS256"},
+		})
+
+	gock.New("http://example.com").
+		Get("/oauth/oauth20/token").
+		Reply(401).
+		BodyString("Access Denied")
+
+	client, err := CreateClient("http://example.com")
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "http://example.com", client.Config.Issuer)
+
+	token, err := client.LoginClientCredentials("TempuratureClient", "SuperSecretSecret")
+
+	assert.Equal(t, "auth service returned: 401 Unauthorized", err.Error())
+	assert.Equal(t, "", token)
+}
